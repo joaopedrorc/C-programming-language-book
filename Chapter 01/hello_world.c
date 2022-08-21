153b39f1ff81c304fd1aeeab277ebfdfa8a9fee6
joaopedrorc <jpc5626@gmail.com>
2022-08-20 23:38:27 -0300

    feat: adding initial chapter 1

---
+++ Chapter 01/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Old version of C code, using gcc -ansi compiler to not cause warnings
+main() {
+  printf("hello, world\n");
+}
+
+//New version of C code, using gcc compiler only
+// int main() {
+//   printf("hello, world\n");
+
+//   return 0;
+// }
